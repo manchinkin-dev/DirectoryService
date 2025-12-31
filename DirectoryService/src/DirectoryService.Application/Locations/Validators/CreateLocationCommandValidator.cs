1c2c44cb355d33518a9a4dcaa053fd04efd8080c
Viktor <manchinkin@gmail.com>
2025-12-31 16:42:01 +0300

    Requests и Commands:     - создана CreateLocationCommand.cs. FluentValidation:     - добавлен валидатор для CreateLocationCommand.cs;     - добавлен вызов валидатора в handler;     - возвращается список ошибок;     - для VO реализована валидация с помощью метода расширения. Реализованы следующие бизнес правила с помощью ограничений в базе данных:     - Нельзя создавать локацию с одним и тем же названием;     - Нельзя создавать локацию на адресе, если такой уже занят.

---
+++ DirectoryService/src/DirectoryService.Application/Locations/Validators/CreateLocationCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using DirectoryService.Application.Validation;
+using DirectoryService.Contracts.Locations;
+using DirectoryService.Domain.Locations;
+using FluentValidation;
+using TimeZone = DirectoryService.Domain.Locations.TimeZone;
+
+namespace DirectoryService.Application.Locations.Validators;
+
+public class CreateLocationCommandValidator : AbstractValidator<CreateLocationCommand>
+{
+    public CreateLocationCommandValidator()
+    {
+        RuleFor(x => x.Request.Name)
+            .MustBeValueObject(LocationName.Create);
+
+        RuleFor(x => x.Request.Address)
+            .MustBeValueObject(dto =>
+                LocationAddress.Create(
+                    dto.Country,
+                    dto.City,
+                    dto.Street,
+                    dto.HouseNumber,
+                    dto.PostalCode));
+
+        RuleFor(x => x.Request.TimeZone)
+            .MustBeValueObject(TimeZone.Create);
+    }
+}
